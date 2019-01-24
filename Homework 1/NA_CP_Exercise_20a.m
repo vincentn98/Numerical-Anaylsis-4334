8c039bdead0a430133e3b2408c442893941b56dd
Vincent Nguyen <vincentn98@gmail.com>
2019-01-24 11:17:31 -0600

    1/24/19

--- Homework 1/NA_CP_Exercise_20a.m
+++ Homework 1/NA_CP_Exercise_20a.m
@@ -1,22 +1,21 @@
 %1.1 exercise 20a
 
-m0 = 3+i;
-n0 = 2+5i;
+m0 = int8(11);
+n0 = int8(5);
 
-x0 = real(m0/n0)
-y0 = real(m0)/real(n0)
+x0 = double(m0/n0)
+y0 = double(m0)/double(n0)
 
-m1 = 1+2i;
-n1 = 1+2i;
+m1 = int8(12);
+n1 = int8(6);
 
-x1 = real(m1/n1)
-y1 = real(m1)/real(n1)
+x1 = double(m1/n1)
+y1 = double(m1)/double(n1)
 
-%x0 ~= y0 because here y0 first takes the real components of m0 and n0,
-%and then divides them resulting in y0 = 3/2 = 1.5. On the other hand,
-%x0 divides m0 and n0 first before taking the real component,
-%resulting in x0 = 0.37931.
-%
-%x1 == y1 because here m1 and n1 are equivalent and non zero,
-%hence when divided by eachother, they equate to 1 regardless of
-%whether or not the real component was taken first.
+%x0 ~= y0 because x0 performs integer division while y perfoms
+%floating point division so x0 produces the rounded value and y0 produces
+%the unrounded value. So y0 would produce the more accurate answer.
+
+%x1 == y1 because in this case the rounded values are equivalent to the
+%unrounded value, which are also divisible integers, hence producing the
+%same value.
