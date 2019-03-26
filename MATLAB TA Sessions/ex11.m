c5dfd0af3e58fd8218dc768750f8520ae455244b
Vincent Nguyen <vincentn98@gmail.com>
2019-03-25 20:02:24 -0500

    3/25/19

--- MATLAB TA Sessions/ex11.m
+++ MATLAB TA Sessions/ex11.m
@@ -3,17 +3,18 @@ clear
 
 %Function Handle is a pointer/reference to a function
 %fh1 = @(x) f1(x); %creates handle referring to f1
-
+%%
 %I1 = integral(fh1,0,1)
 
 %A function defined with a function handle is an anonymous function
-fh2 = @(x) x.^2+x+1;
-I2 = integral(fh2,0,1)
+%fh2 = @(x) x.^2+x+1;
+%I2 = integral(fh2,0,1)
 
 %y1 = f1(1)
 %yh1 = fh1(1)
-yh2 = fh2(1)
+%yh2 = fh2(1)
 
+%%
 %Write a function that takes 2 functions as inputs. Return a function
 %handle that is the product of the two functions.
 %Test with 2 trig functions
@@ -30,8 +31,10 @@ yh2 = fh2(1)
 y3 = a1(@(x) sin(x),@(x) cos(x))
 %yh3 = ah1(1)
 %yh4 = ah2(1)
-x = linspace(0,pi)
+x = linspace(0,pi);
+figure
 plot(x, y3(x))
+title('Example')
 function y = a1(yh3,yh4)
     y =@(x) yh3(x).*yh4(x);
 end
