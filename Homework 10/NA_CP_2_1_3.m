c67f5049285ab8291aa86945f979712c31ab4e1b
Vincent Nguyen <vincentn98@gmail.com>
2019-04-10 22:06:21 -0500

    4/10/19

--- Homework 10/NA_CP_2_1_3.m
+++ Homework 10/NA_CP_2_1_3.m
@@ -10,25 +10,26 @@ n = 3;
 
 for k = 1 : n-1
     for i = k+1 : n
+        b(i) = i+1;
         xmult = a(i,k)/a(k,k);
         a(i,k) = xmult;
         for j = k+1 : n
+            a(i,j) = i + j;
             a(i,j) = a(i,j)-(xmult)*a(k,j);
         end
         b(i) = b(i) - (xmult)*b(k);
     end
 end
 
-a
-b
 x(n) = b(n)/(a(n,n));
 for i = n-1 : 1
-    sim = b(i);
+    sum = b(i);
     for j = i+1 :n
         sum = sum-a(i,j)*x(j);
     end
     x(i) = sum/a(i,i);
 end
 
+a
+b
 x = transpose(x)
-realx = linsolve(A,B)
